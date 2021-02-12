6d19b7a468b96a640f41c8d18d189afc4c43bdf0
asoto <asoto@tuenti.com>
2021-02-12 19:12:03 +0100

    r.sh with more options

--- r.sh
+++ r.sh
@@ -3,11 +3,11 @@ if [ -z $1 ]; then
    echo "give problem as param: `basename $0` <problem>"
 fi
 case $1 in
-    A|B|C|D|E|F|G)
+    A|B|C|D|E|F|G|H|I|J|K)
         folder="$1"
         file=`echo "$1" | tr "[:upper:]" "[:lower:]"`
     ;;
-    a|b|c|d|e|f|g)
+    a|b|c|d|e|f|g|h|i|j|k)
         folder=`echo "$1" | tr "[:lower:]" "[:upper:]"`
         file="$1"
     ;;
