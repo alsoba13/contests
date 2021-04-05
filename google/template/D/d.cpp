0c3533d070626e7de1231e975bbc1346b3dbff96
asoto <asoto@tuenti.com>
2021-04-05 19:06:16 +0200

    google competitions template

---
+++ google/template/D/d.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+	int t;
+	cin >> t;
+	for(int i = 1; i<=t; i++){
+		cout << "Case #" << i << ": ";
+		//slv();
+		cout << f() << '\n';
+	}
+	return 0;
+}
