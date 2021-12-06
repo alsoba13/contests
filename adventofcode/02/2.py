8991da56f0bf11584a94e1a05ce80bba5fa8f7f8
asoto <asoto@tuenti.com>
2021-12-06 22:05:24 +0100

    aoc20: delete old

--- adventofcode/02/2.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-sol1 = sol2 = 0
-for i in range(0, n):
-    inputs = input().rstrip().split(' ')
-    n1, n2, letter, password = [int(inputs[0].split('-')[0]), int(inputs[0].split('-')[1]), inputs[1][0], inputs[2]]
-    sol1 += n1 <= password.count(letter) <= n2
-    sol2 += (password[n1-1] == letter and password[n2-1] != letter) or (password[n1-1] != letter and password[n2-1] == letter)
-print("Problem1: {}\nProblem2: {}".format(sol1, sol2))
