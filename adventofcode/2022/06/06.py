a6e796bad6afe29e4fc4b685dafb26867e032434
alsoba13 <alsoba13@gmail.com>
2022-12-06 10:29:08 +0100

    [aoc22] 06

---
+++ adventofcode/2022/06/06.py
@@ -0,0 +1,8 @@
+def slv(line, n):
+    for i in range(len(line)-n):
+        if len(set(line[i:i+n]))==n:
+            return i+n
+
+line = input().rstrip()
+print("Problem1: {}".format(slv(line, 4)))
+print("Problem1: {}".format(slv(line, 14)))
