1182609f938928279a828179f5a3caf58f6b86a5
alsoba13 <alsoba13@gmail.com>
2022-12-10 12:25:45 +0100

    [aoc22] 10 tweak

--- adventofcode/2022/10/10.py
+++ adventofcode/2022/10/10.py
@@ -2,21 +2,20 @@ def slv(cmds):
     op = None
     x, cycle = 1, 0
     problem1, problem2 = 0, ['\n']
-    while len(cmds)>0 or op is not None:
+    while len(cmds)>0: # or op is not None:
         cycle += 1
         if op is None:
-            cmd, cmds = cmds[0], cmds[1:]
-            if cmd == 'noop': ttl, op = 1, cmd
-            else: ttl, op, incr = 2, cmd.split(' ')[0], int(cmd.split(' ')[1])
+            op, cmds = cmds[0].split(' '), cmds[1:]
+            ttl = len(op)
 
-        if cycle % 40 == 20: problem1 += x*cycle
+        if cycle % 40 == 20: problem1 += x * cycle
         
         problem2.append('#' if abs((cycle-1)%40-x)<=1 else '.')
         if cycle % 40 == 0: problem2.append('\n')
         
         ttl -= 1
         if ttl == 0: 
-            if op == 'addx': x += incr
+            if op[0] == 'addx': x += int(op[1])
             op = None
     return problem1, ''.join(problem2)
 
