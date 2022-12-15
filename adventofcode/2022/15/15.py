5615ace0e3a6abbbf79c6a42baa6d44ef21f9a2e
alsoba13 <alsoba13@gmail.com>
2022-12-15 20:23:23 +0100

    [aoc22] 15 tweak

--- adventofcode/2022/15/15.py
+++ adventofcode/2022/15/15.py
@@ -1,5 +1,4 @@
 # Self timed: * 00:16:52 | ** 00:52:21
-from functools import cmp_to_key
 def merge_intervals(intervals):
     intervals.sort()
     stack = [intervals[0]]
@@ -9,15 +8,13 @@ def merge_intervals(intervals):
     return stack
 
 def get_occupied_intervals(points, N = 4000001):
-    known_beacons = set(beacon for _, beacon in points)
     intervals = [[] for _ in range(N)]
     for sensor, beacon in points:
         distance = abs(beacon[1]-sensor[1])+abs(beacon[0]-sensor[0])
-        for x in range(N):
-            remaining_dist = distance-abs(sensor[1]-x)
-            if remaining_dist<0: continue
+        for y in range(max(0, sensor[1]-distance), min(N, sensor[1]+distance+1)):
+            remaining_dist = distance-abs(sensor[1]-y)
             interval = [sensor[0]-remaining_dist, sensor[0]+remaining_dist+1]
-            intervals[x] = merge_intervals(intervals[x] + [interval])
+            intervals[y] = merge_intervals(intervals[y] + [interval])
     return intervals
 
 def slv1(intervals, points, y=2000000):
@@ -32,8 +29,7 @@ points = []
 while True:
     try: points.append([(int(part.split(', y=')[0].split('x=')[1]), int(part.split(', y=')[1])) for part in input().rstrip().split(":")])
     except EOFError: break
-points_sorted_by_distance = sorted(points, key=cmp_to_key(lambda a, b: abs(b[0][0]-b[1][0])+abs(b[0][1]-b[1][1])-abs(a[0][0]-a[1][0])-abs(a[0][1]-a[1][1])))
-occupied_intervals=get_occupied_intervals(points_sorted_by_distance)
+occupied_intervals=get_occupied_intervals(points)
 
 print("Problem1: {}".format(slv1(occupied_intervals, points)))
 print("Problem2: {}".format(slv2(occupied_intervals)))
