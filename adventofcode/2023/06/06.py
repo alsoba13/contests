492c5669ed164aa0e24256550bab3a32ca9c4665
Alberto <alsoba13@gmail.com>
2023-12-06 11:43:40 +0100

    AOC23/06: 06.py

---
+++ adventofcode/2023/06/06.py
@@ -0,0 +1,15 @@
+def slv(times, distances):
+    sol = 1
+    for time, distance in zip(times, distances):
+        race_sols = 0
+        for v in range(0, time+1):
+            d = (time-v)*v
+            if d > distance: race_sols += 1
+        sol *= race_sols
+    return sol
+
+times = [int(x) for x in input().split(":")[1].split(" ") if x != '']
+distances = [int(x) for x in input().split(":")[1].split(" ") if x != '']
+
+print("Problem1: {}".format(slv(times, distances)))
+print("Problem1: {}".format(slv([int(''.join([str(t) for t in times]))], [int(''.join([str(d) for d in distances]))])))
